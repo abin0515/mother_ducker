480117d0c452969c38033835f85fd8b33151a3b2
abin0515 <384600727@qq.com>
2025-08-12 13:39:52 -0400

    test pipeline

--- backend/user-service/src/main/java/com/example/userservice/UserRepository.java
+++ backend/user-service/src/main/java/com/example/userservice/UserRepository.java
@@ -3,6 +3,7 @@ package com.example.userservice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
